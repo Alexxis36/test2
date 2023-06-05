372db5dd2d3757daca7ca05db72fe98294557aca
Alexxis36 <fedor4encko.dmitr@yandex.ru>
2023-06-05 11:51:02 +0300

    first

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+int* delete_chet(int* mas, int& size) {
+	int col_chet = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (mas[i] % 2 == 0) {
+			col_chet++;
+		}
+	}
+	int new_size = size - col_chet;
+	int* new_mas = new int[new_size];
+	int t = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (mas[i] % 2 != 0) {
+			new_mas[t] = mas[i];
+			t++;
+		}
+	}
+	size = new_size;
+	return new_mas;
+}
+
+int main()
+{
+	int size = 5;
+	int* mas = new int[size];
+	for (int i = 0; i < 5; i++)
+	{
+		mas[i] = i;
+	}
+	int* new_mas = delete_chet(mas, size);
+	for (int i = 0; i < size; i++)
+	{
+		cout << new_mas[i] << " ";
+	}
+}
